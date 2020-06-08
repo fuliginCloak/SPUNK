aa8ef2c0a4616db860164d071e1072cae72b5825
Dominick Persino <59796274+fuliginCloak@users.noreply.github.com>
2020-06-08 12:03:11 -0500

    Add files via upload
    Added bootstrap, some basic styling for now:
    added links for the game and community pages
    still need to add login / register

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-""" main.py for SPUNK """
+from flask import Flask, render_template 
+app = Flask(__name__)
+
+# nav_links = ['The world', 'Community', 'Help', 'Log In/Register']
+
+@app.route('/')
+def home():
+    return render_template('landing.html')
+
+@app.route('/game')
+def the_game():
+    return render_template('the_game.html')
+
+@app.route('/community')
+def the_community():
+    return render_template('community.html')
